49ea4654a5d5e3877d7782649c991cdef3ba5b61
wu <2667497807@qq.com>
2024-05-09 00:24:22 +0800

    完成发布者编写与测试

--- src/example_cpp/src/topic_publisher_01.cpp
+++ src/example_cpp/src/topic_publisher_01.cpp
@@ -7,11 +7,29 @@ class TopicPublisher01:public rclcpp::Node
         TopicPublisher01(std::string name):Node(name)
         {
             RCLCPP_INFO(this->get_logger(), "%s node has started.", name.c_str());
+            //创建发布者
             command_publisher_ = this->create_publisher<std_msgs::msg::String>("command", 10); 
+            //创建定时器，500ms为周期调用timer_callback函数，实现定时发布
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&TopicPublisher01::timer_callback, this));
+            //chrono是c++11的时间库，提供计时、时钟等功能
+            //bind()用来绑定函数调用的参数
         }
 
     private:
-        rclcpp::Publisher<std_msgs:msg::String>::SharedPtr command_publisher_;
+        void timer_callback()
+        {
+            //创建消息
+            std_msgs::msg::String message;
+            message.data = "forward";
+            //打印日志
+            RCLCPP_INFO(this->get_logger(), "Publishing/: '%s'", message.data.c_str());
+            //发布消息
+            command_publisher_->publish(message);
+        }
+        //声明定时器指针
+        rclcpp::TimerBase::SharedPtr timer_;
+        //声明话题发布者指针
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr command_publisher_;
 };
 
 int main(int argc, char **argv)
